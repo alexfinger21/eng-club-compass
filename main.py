aa4f0a24b3d6904fc0d82f44fc1ca1e15bc5a72f
alexfinger21 <cdx9933@gmai1.com>
2024-11-10 21:21:08 -0500

    compass arrow built by a vector instead of a spritesheet, still needs buttons

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import board
 from adafruit_ina219 import INA219
-from displayio import OnDiskBitmap, TileGrid, Group
+from displayio import OnDiskBitmap, TileGrid, Group, Palette
 import terminalio
-import random
+import math
 import time
+import vectorio
 from adafruit_display_text import bitmap_label
 from adafruit_imageload import load
 
@@ -13,23 +14,47 @@ ina219 = INA219(i2c_bus)
 main_group = Group()
 board.DISPLAY.root_group = main_group
 
-imgs = [f"/images/arrows/{x}.bmp" for x in range(1, 7)]
-
-bitmap = OnDiskBitmap("/images/arrows/1.bmp")
-tile_grid = TileGrid(bitmap=bitmap, pixel_shader=bitmap.pixel_shader)
-tile_grid.x = (board.DISPLAY.width - bitmap.width) // 2
-tile_grid.y = (board.DISPLAY.height - bitmap.height) // 2
-
-
 text = "Engineering\nClub"
 text_area = bitmap_label.Label(terminalio.FONT, text=text, scale=3, anchor_point=(0.5, 0.5), color=0x0b78e6)
 
 text_area.anchored_position = (board.DISPLAY.width // 2, board.DISPLAY.height // 2)
 main_group.append(text_area)
-MAX_CURRENT = 40 #mA
+def rotate_point(x, y, abtx, abty, angle):
+    """
+    Rotates a point (x, y) around the origin by a given angle (in radians).
+    """
+    og_angle = math.atan2(y-abty, x-abtx)
+    hyp = math.sqrt((x-abtx)*(x-abtx) + (y-abty)*(y-abty))
+    new = ((math.cos(og_angle+angle)*(hyp)+abtx), (math.sin(og_angle+angle)*(hyp)+abty))
+    return new
 
+def rotate_points(points, center, angle):
+    """
+    Rotates a list of points around the origin by a given angle (in radians).
+    """
+    rotated_points = []
+    for (x, y) in points:
+        rotated_points.append(rotate_point(x, y, center[0], center[1], angle))
+    return rotated_points
+
+path0_w = 25.107
+path0_h = 127.418
+points = [(18.483,125.866),(18.483,115.879),(18.483,105.862),(18.483,95.877),(18.484,85.879),(18.483,75.873),(18.483,65.865),(18.483,55.871),(20.626,47.149),(25.227,41.830),(22.968,32.081),(20.710,22.339),(18.452,12.595),(16.194,2.853),(11.148,5.861),(8.602,15.547),(6.063,25.207),(3.521,34.879),(0.976,44.560),(8.948,47.422),(9.917,57.077),(9.917,67.078),(9.917,77.083),(9.917,87.081),(9.917,97.079),(9.917,107.074),(9.917,117.068),(10.274,127.011),]
+
+def flatten_points(point_list):
+    return [(int(x[0]), int(x[1])) for x in point_list]
+
+palette = Palette(1)
+palette[0] = 0xFFFFFF
+MAX_CURRENT = 50 #mA
+
+polygon_shape = vectorio.Polygon(
+        pixel_shader=palette,
+        points=flatten_points(points),
+        x=100, y=8
+)
 time.sleep(1.5)
-main_group.append(tile_grid)
+main_group.append(polygon_shape)
 main_group.remove(text_area)
 main_group.append(text_area)
 
@@ -43,19 +68,11 @@ while True:
     #print(current/MAX_CURRENT)     
     power = ina219.power  # power in watts
     blinka_img = None
-    if (current/MAX_CURRENT <= 0.5):
-        blinka_img = round(5*current/MAX_CURRENT*2)
-        #main_group.rotation = 0
-        tile_grid.flip_x = True
-        tile_grid.flip_y = True
-    else:
-        blinka_img = round(5*2*(current/MAX_CURRENT-0.5))
-        tile_grid.flip_x = False
-        tile_grid.flip_y = False
-    #print(imgs[blinka_img])
-    bmp = OnDiskBitmap(imgs[blinka_img])
-    tile_grid.bitmap = bmp
-    tile_grid.pixel_shader = bmp.pixel_shader
-    text_area.text = f"{round(current)} mA     {round(180-180*current/MAX_CURRENT)} dg"
-    time.sleep(0.1)
+    #rotated_points = rotate_points(points, (board.DISPLAY.width//2, board.DISPLAY.height//2), current/MAX_CURRENT*2*math.pi)
+    # 11.5 on x and half of the display height on y is the center of the arrow
+    rotated_points = rotate_points(points, (path0_w/2, path0_h/2), current/MAX_CURRENT*2*math.pi)
+    polygon_shape.points = flatten_points(rotated_points)
+    text_area.text = f"{round(current)} mA     {round(360*current/MAX_CURRENT)} dg"
+    time.sleep(0.01)
+
 
