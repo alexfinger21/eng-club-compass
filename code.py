7a741a293d2869d65ef6ecfc4d4206b28b88e589
alexfinger21 <cdx9933@gmail.com>
2026-05-17 13:56:28 -0400

    updated unit; work in progress

--- code.py
+++ code.py
@@ -1,15 +1,13 @@
 import board
 from adafruit_ina219 import INA219
-from displayio import OnDiskBitmap, TileGrid, Group, Palette
+from displayio import Group, Palette
 import digitalio
 import terminalio
-import pwmio
 import math
 import alarm
 import time
 import vectorio
 from adafruit_display_text import bitmap_label
-from adafruit_imageload import load
 
 i2c_bus = board.I2C()  # uses board.SCL and board.SDA
 ina219 = INA219(i2c_bus)
@@ -25,15 +23,6 @@ button = digitalio.DigitalInOut(board.BOOT0)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
-sleep_pin = digitalio.DigitalInOut(board.A1)
-sleep_pin.direction = digitalio.Direction.INPUT
-sleep_pin.pull = digitalio.Pull.DOWN
-
-slp = True
-
-if (sleep_pin.value):
-    slp = False
-
 button_toggle = False
 prev_val_btn = False
 
@@ -46,12 +35,14 @@ main_group.append(deg_text)
 ma_text = bitmap_label.Label(terminalio.FONT, text=text, scale=4, anchor_point=(0.5, 0.5), color=0x0b78e6)
 ma_text.anchored_position = (display.width // 2, display.height // 2)
 
+'''
 def deep_sleep():
     print("here")
     sleep_pin.deinit()
     pin_alarm = alarm.pin.PinAlarm(pin=board.A1, value=True, pull=True)
     display.brightness = 0
     alarm.exit_and_deep_sleep_until_alarms(pin_alarm)
+'''
     
 def rotate_point(x, y, abtx, abty, angle):
     og_angle = math.atan2(y-abty, x-abtx)
@@ -90,12 +81,12 @@ main_group.append(deg_text)
 deg_text.anchored_position = (display.width, display.height - 12)
 deg_text.scale = 2
 
-if (slp):
-    deep_sleep()
+letter_units = ['uA', 'mA', 'A']
 
 while True:
-    #pull up so reverse
-    if button.value == True and prev_val_btn == False:
+    # pull up resistor so reverse
+
+    if button.value is True and prev_val_btn is False:
         if not button_toggle:
             main_group.remove(polygon_shape)
             main_group.remove(deg_text)
@@ -104,13 +95,24 @@ while True:
             main_group.append(polygon_shape)
             main_group.append(deg_text)
             main_group.remove(ma_text)
+
         button_toggle = not button_toggle
+
     prev_val_btn = button.value
 
     bus_voltage = ina219.bus_voltage  # voltage on V- (load side)
+
     shunt_voltage = ina219.shunt_voltage  # voltage between V+ and V- across the shunt
-    current = max(0, min(MAX_CURRENT, ina219.current))  # current in mA
-    #print(current/MAX_CURRENT)
+
+    current = ina219.current * 1000  # current in uA
+    unit = 0
+
+    while current >= 1000:
+        unit += 1
+        current /= 1000
+
+    unit = min(unit, letter_units.size - 1)
+
     power = ina219.power  # power in watts
     blinka_img = None
     #rotated_points = rotate_points(points, (board.DISPLAY.width//2, board.DISPLAY.height//2), current/MAX_CURRENT*2*math.pi)
@@ -118,16 +120,11 @@ while True:
     rotated_points = rotate_points(points, (path0_w/2, path0_h/2), (1-current/MAX_CURRENT)*math.pi/2)
     polygon_shape.points = flatten_points(rotated_points)
     new_deg_text = f"{round(90*(1-current/MAX_CURRENT))} dg  "
-    new_ma_text = f"{round(current/1000)} A\n{round(current)} mA"
+    new_ma_text = f"{current} {letter_units[unit]}\n{round(current)} mA"
     if new_deg_text != deg_text.text:
         deg_text.text = new_deg_text
 
     if new_ma_text != ma_text.text:
         ma_text.text = new_ma_text
-
-    
-    if (not sleep_pin.value):
-        deep_sleep()
-        break
     
     time.sleep(0.1)
